86b7a06975bfb7620325c7bc662abdeb544c52e3
Indraneel Vairagare <indraneel207@gmail.com>
2023-06-23 14:49:53 -0400

    Init codebase

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+import './App.css'
 
 function App() {
+  const [flag, setFlag] = useState(null)
+  const [items, setItems] = useState([])
+  const [intervalId, setIntervalId] = useState(null)
+
+  useEffect(() => {
+    fetch('https://wgg522pwivhvi5gqsn675gth3q0otdja.lambda-url.us-east-1.on.aws/70726f')
+      .then((response) => response.text())
+      .then((text) => setFlag(text))
+      .catch((error) => console.error(error))
+  }, [])
+
+  useEffect(() => {
+    let i = 0
+    const timeId = setInterval(() => {
+      const itemsToSet = flag.split('').slice(0, i)
+      setItems(itemsToSet)
+      i += 1
+    }, 500)
+    setIntervalId(timeId)
+    return () => {
+      clearInterval(timeId)
+    }
+  }, [flag])
+
+  useEffect(() => {
+    if (intervalId && items?.length >= flag?.length) clearInterval(intervalId)
+  }, [items, intervalId, flag])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        {flag && items ? (
+          <ul>
+            {items.map((each) => (
+              <li key={each}>{each}</li>
+            ))}
+          </ul>
+        ) : (
+          <p>Loading...</p>
+        )}
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
